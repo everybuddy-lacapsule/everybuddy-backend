10add76ead4d933c461a15a6753d514083f85dab
Epsy0 <serrano.lucas@gmail.com>
2022-08-01 12:28:55 +0200

    première étape login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var UserModel = require("../models/users");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/sign-in", async function (req, res, next) {
+  var saved = false;
+  var user = await UserModel.findOne({
+    email: req.body.email,
+  });
+  if (user.email === req.body.email && user.pwd === req.body.pwd) {
+    saved = true;
+  }
+  res.json({ saved });
 });
 
 module.exports = router;
