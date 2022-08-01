12001bb5ddc1d89f4fa156438f28a9837e621b99
WilliamWerle <williamwiut@gmail.com>
2022-08-01 17:49:35 +0200

    'checkemail'

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,6 @@ var UserModel = require("../models/users");
 var DiscussionModel = require("../models/discussions");
 var BuddyModel = require("../models/buddies");
 
-/* GET home page. */
-/*
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
- */
+
 
 module.exports = router;
