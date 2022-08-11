cc0d8097da0352177f1f0cd9c2209d492f0309db
Thanh Qui <nguyenhtqui@gmail.com>
2022-08-11 23:25:48 +0200

    fix self discussion and userID undefined

--- routes/discussions.js
+++ routes/discussions.js
@@ -5,12 +5,25 @@ const DiscussionModel = require("../models/discussions");
 
 /*---POST: create a discussion for users if not exist---*/
 router.post("/createDiscussion", async function (req, res, next) {
+  console.log(req.body.senderID);
+  console.log(req.body.receiverID);
+  let discussion;
   try {
-    // Find discussion in DB (WITHOUT regard to ORDER or other elements in the array by using $all operator)
-    const discussion = await DiscussionModel.findOne({
-      memberIDs: { $all: [req.body.senderID, req.body.receiverID] },
-    });
+    if (req.body.senderID === req.body.receiverID) {
+      discussion = await DiscussionModel.findOne({
+        memberIDs: [req.body.senderID, req.body.receiverID],
+      });
+      // If discussion exist => send discussionID to Front
+      console.log("Discussion", discussion);
+    } else {
+      // Find discussion in DB (WITHOUT regard to ORDER or other elements in the array by using $all operator)
+      discussion = await DiscussionModel.findOne({
+        memberIDs: { $all: [req.body.senderID, req.body.receiverID] },
+      });
+    }
+
     // If discussion exist => send discussionID to Front
+    console.log("Discussion", discussion);
     if (discussion) {
       res.status(200).json(discussion._id);
       // Else => create a new discussion in DB THEN send discussionID to Front
